6c188a813e77fc5ab402e2f86d5b3eeb524d0a78
Dennis Kurochkin <dennis.kurochkin@gmail.com>
2020-06-15 11:34:01 +0300

    Add a new item controller

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,17 +1,20 @@
 var budgetController = (function () {
 
+  // Expense object prototype
   var Expense = function (id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
 
+  // Income object prototype
   var Income = function (id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
 
+  // Data structure
   var data = {
     allItems: {
       exp: [],
@@ -24,8 +27,32 @@ var budgetController = (function () {
   }
 
   return {
-    addItem: function(type, desc, val) {
-      // if (type )
+    // Function to add item
+    addItem: function (type, desc, val) {
+      var id, newItem;
+
+      // Create an id for a new item
+      id = 0;
+
+      if (data.allItems[type].length > 0)
+        id = data.allItems[type][data.allItems[type].length - 1].id + 1;
+
+      // Create new item based on its type
+      if (type === 'inc') {
+        newItem = new Income(id, desc, val);
+      } else if (type === 'exp') {
+        newItem = new Expense(id, desc, val);
+      }
+
+      // Push new item to the data structure
+      data.allItems[type].push(newItem);
+
+      // Return new item
+      return newItem;
+    },
+    // Function to test data
+    testing: function () {
+      console.log(data);
     }
   }
 
@@ -75,7 +102,21 @@ var controller = (function (budgetCtrl, UICtrl) {
 
 
   var ctrlAddItemHandler = function () {
-    var input = UICtrl.getInput();
+    var input, newItem;
+
+    // Get input data
+    input = UICtrl.getInput();
+
+    // Add item to the data structure
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
+    // Add item to the UI
+
+    // Calculate tht budget
+
+    // Display the budget on the UI
+
+    return input;
   };
 
   return {
