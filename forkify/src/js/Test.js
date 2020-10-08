5a87d0d36a0c74496679bdf3830703609f980baf
Dennis Kurochkin <dennis.kurochkin@gmail.com>
2020-10-08 11:31:10 +0300

    Add API handling

--- forkify/src/js/Test.js
+++
@@ -1,1 +0,0 @@
-export default 'Module have been exported';
--- forkify/src/js/index.js
+++ forkify/src/js/index.js
@@ -1,3 +1,19 @@
-import x from './Test';
+import axios from 'axios';
 
-console.log(x);
+async function getResults(query) {
+  const apiKey = '6522c1a90ff64af48ed047a91ad84c99';
+
+  try {
+    const result = await axios(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}`);
+    const recipes = result.data.results;
+    console.log(recipes);
+  } catch (error) {
+    alert(error);
+  }
+
+}
+
+getResults('potato');
+
+// API key 6522c1a90ff64af48ed047a91ad84c99
+// https://api.spoonacular.com/recipes
